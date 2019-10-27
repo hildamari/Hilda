a99ea46e6c32f933a9a1fa75bc4c823440ea1ab6
Kaoru <KunoichiZ@users.noreply.github.com>
2019-10-27 15:57:53 -0500

    Create ahshit.js

---
+++ commands/misc/ahshit.js
@@ -0,0 +1,23 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class AhShitCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'ahshit',
+      group: 'misc',
+      memberName: 'ahshit',
+      description: 'Replies with AhShit image',
+      examples: ['ahshit'],
+      guildOnly: true
+    });
+  }
+
+  run (msg) {
+    const ahShitEmbed = new MessageEmbed()
+	  .setImage('https://i.imgur.com/SgNoXXY.png');
+    
+    msg.channel.send(ahShitEmbed);
+    // <:Hilda:610937234337693716>
+  }
+};
