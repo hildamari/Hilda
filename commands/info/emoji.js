390d3e9feeb21eba763a42a68209c195a3dab396
Kaoru <KunoichiZ@users.noreply.github.com>
2019-11-11 01:26:53 -0600

    Update emoji.js

--- commands/info/emoji.js
+++ commands/info/emoji.js
@@ -17,9 +17,9 @@ module.exports = class EmojiCommand extends Command {
     const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
     if(emojiList.length > 2000) {
       msg.say("There are too many emojis to list!");
-      console.log(emojiList.length);
+      // console.log(emojiList.length);
     } else {
-      console.log(emojiList.length);
+      // console.log(emojiList.length);
       return msg.channel.send(emojiList);
     }
   }
