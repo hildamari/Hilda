2c3193308b3dd57ba3f59df58339f96e3cd00157
Kaoru <KunoichiZ@users.noreply.github.com>
2019-11-18 01:29:24 -0600

    Update hilda.js

--- hilda.js
+++ hilda.js
@@ -1,13 +1,29 @@
 const path = require('path');
 const { Client, SyncSQLiteProvider } = require('discord.js-commando');
 const Database = require('better-sqlite3');
+const { Structures } = require('discord.js');
 const sqlite = require('sqlite');
-
 require('dotenv').config({path: path.join(__dirname, 'data/.env')});
+
+Structures.extend('Guild', Guild => {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null
+      };
+    }
+  }
+  return MusicGuild;
+});
 
 const client = new Client({
   owner: '147800635046232064',
   commandPrefix: process.env.PREFIX,
+  unknownCommandResponse: false,
   disableEveryone: true,
   selfbot: false,
   presence: {
@@ -32,6 +48,7 @@ client.registry
         ['info', 'info commands'],
         ['links', 'link commands'],
         ['misc', 'misc commands'],
+        ['music', 'music commands'],
         ['owner', 'owner commands']
     ])
     .registerDefaultGroups()
