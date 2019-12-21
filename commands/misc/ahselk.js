b53e6019f7db324d85d1d5a554e7ef749757ec75
Kaoru <KunoichiZ@users.noreply.github.com>
2019-12-21 17:15:34 -0600

    Update ahselk.js

--- commands/misc/ahselk.js
+++ commands/misc/ahselk.js
@@ -1,5 +1,4 @@
 const { Command, MessageAttachment } = require('discord.js-commando');
-const { MessageEmbed } = require('discord.js');
 
 module.exports = class AhSelkCommand extends Command {
   constructor (client) {
@@ -18,7 +17,7 @@ module.exports = class AhSelkCommand extends Command {
     // // .setImage('https://i.imgur.com/Vu5IdfS.png')
     // .attachFiles(['./images/ahselk.png']);
     // const attachment = new MessageAttachment('./images/ahselk.png');
-    msg.channel.send({ files: ["./images/ahselk.png"] });
+    // msg.channel.send({ files: ["./images/ahselk.png"] });
     if(msg.guild.id == 649054519556308992) {
       if(msg.channel.id != 649489304514461706) {
         msg.say("You cannot use this command in a non-nsfw channel! Please go to <#650489311095029780>")
