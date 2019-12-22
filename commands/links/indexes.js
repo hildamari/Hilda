4a440715c28ee113cff3692723a37569f7b4a928
Kaoru <KunoichiZ@users.noreply.github.com>
2019-12-22 16:16:23 -0600

    Update indexes.js

--- commands/links/indexes.js
+++ commands/links/indexes.js
@@ -19,7 +19,7 @@ module.exports = class IndexesCommand extends Command {
             .setTitle("FETH Files and Indexes")
             .addField("URL 1", "https://docs.google.com/spreadsheets/d/18bCCrsHwyAU-JSlpvaulVos3j8dtPBr0mDB-vLWib54/edit#gid=1616992074")
             .addField("URL 2", "https://docs.google.com/spreadsheets/d/14HT0w3qaRPknzxeHysswVMUfry8J2G_RHY8k48Nmy5w/edit#gid=0")
-            .setDescription("Create and edit Koei Tecmo G1T files using a GUI");
+            .setDescription("Files and Indexes Google Spreadsheets");
 
         msg.channel.send(indexesEmbed);
     }
