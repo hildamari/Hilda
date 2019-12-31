6db7aa37d3a7b1e73550f2b4280110c59bfc1db2
Kaoru <KunoichiZ@users.noreply.github.com>
2019-12-31 00:10:22 -0600

    Start rewrite

---
+++ src/Hilda.js
@@ -0,0 +1,45 @@
+const { Client } = require('klasa');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, 'data/.env')});
+
+Client.defaultUserSchema.add('experience', 'Integer', {
+    default: 1,
+    configurable: false
+});
+    
+Client.defaultUserSchema.add('level', 'Integer', {
+    default: 0,
+    configurable: false
+});
+
+Client.defaultUserSchema.add('orbs', 'Integer', {
+    default: 0,
+    configurable: false
+});
+
+Client.defaultUserSchema.add('dailyTime', 'String', {
+    default: '0',
+    configurable: false
+});
+
+Client.defaultUserSchema.add('fc', 'String', {
+    default: '',
+    configurable: false
+});
+
+const HildaClient = new Client ({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    fetchAllMembers: false,
+    createPiecesFolders: false,
+    prefix: process.env.PREFIX,
+    commandEditing: true,
+    disableEveryone: true,
+    ignoreBots: true,
+    presence: { activity: { name: 'Uno', type: 'PLAYING' } },
+    typing: false,
+    prefixCaseInsensitive: true,
+    owners: ['147800635046232064'],
+    readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`});
+
+HildaClient.login(process.env.TOKEN);
