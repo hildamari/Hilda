a1885878cdd2ed4bea2498d151a7146196c588d2
Kaoru <KunoichiZ@users.noreply.github.com>
2019-12-31 00:12:09 -0600

    Add Owner commands

---
+++ src/commands/Owner/echo.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			runIn: ['text'],
+			description: 'Send a message to a channel through the bot.',
+			usage: '[channel:channel] <message:...string>',
+            usageDelim: ' ',
+            permissionLevel: 9
+		});
+	}
+
+	async run(msg, [channel = msg.channel, message]) {
+		if (channel.guild !== msg.guild) throw 'You can\'t echo in other servers!';
+		if (!channel.postable) throw 'The selected channel is not postable.';
+		return channel.send(message);
+	}
+
+};
---
+++ src/commands/Owner/setavatar.js
@@ -0,0 +1,19 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { description: 'Sets the avatar of the bot.' });
+    }
+
+    async run(msg) {
+        try {
+            await this.client.user.setAvatar('https://cdn.discordapp.com/attachments/614902147565486111/643891293839163434/Hilda_Maid.png');
+            
+            return msg.send('Avatar changed!');
+          } catch (err) {
+            return console.error(err);
+          }
+    }
+
+};
