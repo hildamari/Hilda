8dcafdca3607a66052b098e6dd894cf6b6d2acb3
Kaoru <KunoichiZ@users.noreply.github.com>
2019-12-31 00:10:36 -0600

    Create message.js

---
+++ src/events/message.js
@@ -0,0 +1,20 @@
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, { 
+			event: 'message'
+		});
+	}
+
+	run(msg) {
+		const imbaby = this.client.emojis.find(emoji => emoji.name === "ImBaby");
+		if(msg.author.id == "650566385881317376") {
+            if(msg.content.startsWith('<:Marianne:658006511578251266> I don\'t have uno')) {
+				msg.send(`${imbaby} Everyone has uno, dipshit.`)
+            }
+        }
+	}
+
+};
