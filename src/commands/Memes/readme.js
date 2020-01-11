f51b154f581e48b4cd5c9aefaa369e22b587ddfd
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-11 02:20:32 -0600

    Update readme.js

--- src/commands/Memes/readme.js
+++ src/commands/Memes/readme.js
@@ -3,7 +3,7 @@ const { Command } = require('klasa');
 module.exports = class extends Command {
 
     constructor(...args) {
-        super(...args, { description: 'Sends with Moonling\'s readme image' });
+        super(...args, { description: 'Sends Moonling\'s readme image' });
     }
 
     run(msg) {
