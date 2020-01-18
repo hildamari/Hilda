b5ca0c0c02c41dcc5bd7caab2d2414a3f0665f88
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-17 23:30:58 -0600

    Create joesmad.js

---
+++ src/commands/Memes/joesmad.js
@@ -0,0 +1,23 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Sends the JoesMad image' 
+        });
+    }
+
+    run(msg) {
+        if(msg.guild.id == 649054519556308992) {
+            if(msg.channel.id != 649489304514461706) {
+              msg.send("You cannot use this command in a non-nsfw channel! Please go to <#650489311095029780>")
+            } else {
+                msg.channel.send({ files: ["./assets/images/JoesMad.jpg"] });
+            }
+        } else {
+            msg.channel.send({ files: ["./assets/images/JoesMad.jpg"] });
+        }
+    }
+
+};
