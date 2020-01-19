2a6253656e5d73dae85baa92a6026f78caf7d72a
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-18 20:28:20 -0600

    Update doesmad.js

--- src/commands/Memes/doesmad.js
+++ src/commands/Memes/doesmad.js
@@ -9,15 +9,7 @@ module.exports = class extends Command {
     }
 
     run(msg) {
-        if(msg.guild.id == 649054519556308992) {
-            if(msg.channel.id != 649489304514461706) {
-              msg.send("You cannot use this command in a non-nsfw channel! Please go to <#650489311095029780>")
-            } else {
-                msg.channel.send({ files: ["./assets/images/DoesMad.jpg"] });
-            }
-        } else {
-            msg.channel.send({ files: ["./assets/images/DoesMad.jpg"] });
-        }
+        msg.channel.send({ files: ["./assets/images/DoesMad.jpg"] });
     }
 
 };
