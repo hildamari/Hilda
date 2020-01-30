215f64070a74e8919f5951628e6d799fa846462c
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-30 01:46:51 -0600

    Create queue.js

---
+++ src/commands/Music/queue.js
@@ -0,0 +1,30 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags')
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            aliases: ['song-list', 'next-songs'],
+            description: 'Display the song queue'
+        });
+    }
+
+    run(msg) {
+        if (msg.guild.musicData.queue.length == 0)
+            return msg.send(`There are no songs in queue! Add some with ${msg.guild.settings.get('prefix')}play or ${msg.guild.settings.get('prefix')}add`);
+        const titleArray = [];
+        msg.guild.musicData.queue.map(obj => {
+            titleArray.push(obj.title);
+        });
+        var queueEmbed = new MessageEmbed()
+            .setColor('#ff7373')
+            .setTitle('Music Queue')
+            for (let i = 0; i < titleArray.length; i++) {
+                queueEmbed.addField(`${i + 1}:`, `${titleArray[i]}`);
+            }
+        return msg.send(queueEmbed);
+    }
+    
+};
