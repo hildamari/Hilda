adcabd0268b6d72ff9fd20d6162f6575c664d681
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-30 01:30:01 -0600

    Update hilda command

--- src/commands/Memes/hilda.js
+++ src/commands/Memes/hilda.js
@@ -5,13 +5,27 @@ module.exports = class extends Command {
     constructor(...args) {
         super(...args, { 
             aliases: ['chant'],
-            description: 'Chants HIL-DA HIL-DA' 
+            description: 'Chants HIL-DA HIL-DA and plays the voice clip' 
         });
     }
 
-    run(msg) {
-        const hilda = this.client.emojis.find(emoji => emoji.name === "Hilda");
-        return msg.send(`${hilda} HIL-DA HIL-DA ${hilda}`);
+    async run(msg) {
+        if (!msg.guild) return;
+
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (msg.member.voice.channel) {
+            const connection = await msg.member.voice.channel.join();
+            const dispatcher = connection.play('./assets/sounds/hilda.wav');
+
+            dispatcher.on('finish', () => {
+                console.log('Finished playing!');
+            });
+            const hilda = this.client.emojis.find(emoji => emoji.name === "Hilda");
+            msg.send(`${hilda} HIL-DA HIL-DA ${hilda}`);
+        } else {
+            const hilda = this.client.emojis.find(emoji => emoji.name === "Hilda");
+            msg.send(`${hilda} HIL-DA HIL-DA ${hilda}`);
+        }
     }
 
 };
