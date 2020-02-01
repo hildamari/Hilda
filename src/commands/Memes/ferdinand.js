47dde0c13ab961a003956b9b141502d6a3627586
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-31 18:23:35 -0600

    Update ferdinand.js

--- src/commands/Memes/ferdinand.js
+++ src/commands/Memes/ferdinand.js
@@ -15,12 +15,10 @@ module.exports = class extends Command {
         if (msg.member.voice.channel) {
             const connection = await msg.member.voice.channel.join();
             const dispatcher = connection.play('./assets/sounds/ferdinand.wav');
-
-            dispatcher.on('error', () => {
-                console.error
-            })
+            
             dispatcher.on('finish', () => {
                 console.log('Finished playing!');
+                msg.guild.me.voice.channel.leave();
             });
         } else {
             msg.reply('You need to join a voice channel first!');
