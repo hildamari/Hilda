e431154fb34476bd64e247ce3b59987ead589985
Kaoru <KunoichiZ@users.noreply.github.com>
2020-02-22 02:02:47 -0600

    Update ferdinand.js

--- src/commands/Memes/ferdinand.js
+++ src/commands/Memes/ferdinand.js
@@ -10,16 +10,22 @@ module.exports = class extends Command {
     
     async run (msg) {
         if (!msg.guild) return;
+        const serverQueue = msg.guild.musicData.queue.get(msg.guild.id);
 
         // Only try to join the sender's voice channel if they are in one themselves
         if (msg.member.voice.channel) {
-            const connection = await msg.member.voice.channel.join();
-            const dispatcher = connection.play('./assets/sounds/ferdinand.wav');
+            if(!serverQueue){
+                const connection = await msg.member.voice.channel.join();
+                const dispatcher = connection.play('./assets/sounds/ferdinand.wav');
+                
+                dispatcher.on('finish', () => {
+                    console.log('Finished playing!');
+                    msg.guild.me.voice.channel.leave();
+                });
+            } else {
+                msg.send("You can't use this command while a song is playing!")
+            }
             
-            dispatcher.on('finish', () => {
-                console.log('Finished playing!');
-                msg.guild.me.voice.channel.leave();
-            });
         } else {
             msg.reply('You need to join a voice channel first!');
         }
