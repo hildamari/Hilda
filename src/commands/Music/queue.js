69f72eabc5536e8197ad3af0c5b6ad0897edce47
Kaoru <KunoichiZ@users.noreply.github.com>
2020-02-22 11:31:13 -0600

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
 
     run(msg) {
         const serverQueue = msg.guild.musicData.queue.get(msg.guild.id);
-        console.log(serverQueue)
+        // console.log(serverQueue.songs)
 
         if (serverQueue.songs.length == 0)
             return msg.send(`There are no songs in queue! Add some with ${msg.guild.settings.get('prefix')}play or ${msg.guild.settings.get('prefix')}add`);
@@ -27,7 +27,7 @@ module.exports = class extends Command {
             .setColor('#ff7373')
             .setTitle(':musical_note: Music Queue :musical_note: ')
             for (let i = 0; i < titleArray.length; i++) {
-                queueEmbed.addField(`Song ${i + 1}:`, `${titleArray[i]} by ${artistArray[i]}`);
+                queueEmbed.addField(`Song ${i + 1}:`, `**${titleArray[i]}** by ${artistArray[i]}`);
             }
         return msg.send(queueEmbed);
     }
