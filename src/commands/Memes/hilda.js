fd7346652981c73a664d47d287fea852bdd979ac
Kaoru <KunoichiZ@users.noreply.github.com>
2020-02-22 02:02:40 -0600

    Update hilda.js

--- src/commands/Memes/hilda.js
+++ src/commands/Memes/hilda.js
@@ -11,18 +11,24 @@ module.exports = class extends Command {
 
     async run(msg) {
         if (!msg.guild) return;
+        const serverQueue = msg.guild.musicData.queue.get(msg.guild.id);
 
         // Only try to join the sender's voice channel if they are in one themselves
         if (msg.member.voice.channel) {
-            const connection = await msg.member.voice.channel.join();
-            const dispatcher = connection.play('./assets/sounds/hilda.wav');
-
-            dispatcher.on('finish', () => {
-                console.log('Finished playing!');
-                msg.guild.me.voice.channel.leave();
-            });
-            const hilda = this.client.emojis.find(emoji => emoji.name === "Hilda");
-            msg.send(`${hilda} HIL-DA HIL-DA ${hilda}`);
+            if(!serverQueue){
+                const connection = await msg.member.voice.channel.join();
+                const dispatcher = connection.play('./assets/sounds/hilda.wav');
+    
+                dispatcher.on('finish', () => {
+                    console.log('Finished playing!');
+                    msg.guild.me.voice.channel.leave();
+                });
+                const hilda = this.client.emojis.find(emoji => emoji.name === "Hilda");
+                msg.send(`${hilda} HIL-DA HIL-DA ${hilda}`);
+            } else {
+                msg.send("You can't use this command while a song is playing!")
+            }
+            
         } else {
             const hilda = this.client.emojis.find(emoji => emoji.name === "Hilda");
             msg.send(`${hilda} HIL-DA HIL-DA ${hilda}`);
