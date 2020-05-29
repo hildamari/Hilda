fa78a8acda4b2cf31150e3f1b95469f52ebc7cf6
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-28 23:44:43 -0500

    Update role.js

--- src/monitors/role.js
+++ src/monitors/role.js
@@ -1,12 +1,12 @@
 const { Monitor } = require('klasa');
-const Fuse = require('fuse.js');
-var fs = require("fs");
-const { resolve, join } = require("path");
-let messages = JSON.parse(fs.readFileSync(resolve(join(__dirname, "../../src/data/messages.json"))))
+// const Fuse = require('fuse.js');
+// var fs = require("fs");
+// const { resolve, join } = require("path");
+// let messages = JSON.parse(fs.readFileSync(resolve(join(__dirname, "../../src/data/messages.json"))))
 
-var messageOptions = {
-    keys: ['id']
-}
+// var messageOptions = {
+//     keys: ['id']
+// }
 
 module.exports = class extends Monitor {
 
@@ -19,17 +19,17 @@ module.exports = class extends Monitor {
         // If the message was not sent in a TextChannel, ignore it.
         if (!msg.guild) return;
         let member = msg.member;
-        var random = Math.floor(Math.random() * 3) + 1;
-        var randomTxt = random.toString();
-        var messagesFuse = new Fuse(messages.messages, messageOptions); 
-        var result = messagesFuse.search(randomTxt);
+        // var random = Math.floor(Math.random() * 3) + 1;
+        // var randomTxt = random.toString();
+        // var messagesFuse = new Fuse(messages.messages, messageOptions); 
+        // var result = messagesFuse.search(randomTxt);
 
         if(msg.guild.id == 354835055623012352) {
             let memberRole = msg.guild.roles.find(roles => roles.name === "members");
             if(!member.roles.has(memberRole.id)) {
                 msg.member.roles.add(memberRole).catch(console.error);
                 
-                msg.send("<@" + msg.author.id + ">" + result[0].msg)
+                // msg.send("<@" + msg.author.id + ">" + result[0].msg)
             }
         }
 
