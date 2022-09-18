e28e728fe91cfd181267d2e071391b2cf6f41ba4
KunoichiZ <KunoichiZ@users.noreply.github.com>
2022-09-18 00:58:53 -0500

    remove old files

--- src/lib/HildaCommand.ts
+++
@@ -1,8 +0,0 @@
-import { Command, CommandOptions, PieceContext } from '@sapphire/framework';
-
-export default abstract class HildaCommand extends Command {
-
-	public constructor(context: PieceContext, { name, ...options }: CommandOptions) {
-		super(context, { name, ...options });
-	}
-}
